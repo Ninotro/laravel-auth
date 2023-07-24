f6916b928b4a61f4bf2d80e4aac737cd876c5d4e
Ninotro <ninotroia96@gmail.com>
2023-07-24 15:36:28 +0200

    Completed

--- resources/views/projects/show.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>Dettaglio Progetto</h1>
-    <p><strong>Nome: </strong>{{ $project->name }}</p>
-    <p><strong>Descrizione: </strong>{{ $project->description }}</p>
-    <p><strong>Cliente: </strong>{{ $project->client }}</p>
-    <p><strong>Data di Inizio: </strong>{{ $project->start_date }}</p>
-@endsection
